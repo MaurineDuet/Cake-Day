8173a85523a99ed34d57b202870e05d1891e6499
Maurine Duet <duet_maurine@MacBook-Air-de-Maurine.local>
2023-09-16 11:57:50 +0900

    Essai deux sur les chemins des images

--- js/theme.js
+++ js/theme.js
@@ -7,12 +7,12 @@ document.body.setAttribute("theme", color)
 
 const drinkImage = document.querySelector("[data-drink-img]")
 if (drinkImage) {
-    drinkImage.src = `./Img/drink_${color}.png`
+    drinkImage.src = `../Img/drink_${color}.png`
 }
 
 const sideImage = document.querySelector("[data-side-img]")
 if (sideImage) {
-    sideImage.src = `./Img/side_${color}.png`
+    sideImage.src = `../Img/side_${color}.png`
 }
 
 //localStorage.setItem("theme", color)
